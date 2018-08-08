fb4e8953656a20a715c25e9b5820a8e49cdfe58e
Nilesh Mittal <GSC-30281@GSLAB.COM>
2018-08-08 19:51:53 +0530

    ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT | 4000);
+app.listen(process.env.PORT || 4000);
 
 app.get('/*',(req, res)=>{
     res.sendFile(path.join(__dirname + '/public/index.html'));
